077142fbf1cb04b6855b866857a2e6f3648622ba
Henrique V <henriquevermelho@fetch.ly>
2022-07-28 03:43:24 -0300

    Final conda profile on powershell

---
+++ profile.ps1
@@ -0,0 +1,6 @@
+# This file is executed from 'D:\Users\Mestre\Documents\WindowsPowerShell\profile.ps1'
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+#(& "C:\Users\henri\Miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
